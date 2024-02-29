03d19e38166a3ed24ceb41575c06778a746df5db
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-29 11:15:32 +0300

    first dart program

---
+++ main.dart
@@ -0,0 +1,18 @@
+// Variables in data
+// data types in dart
+
+void main()
+{
+
+  print("Hello World!");
+  // bool isMarried = true;
+  
+  
+  // int age = 25;
+  // double height = 1.78;
+  // print(age + height);
+
+  // String name = "John Doe";
+  // print("name");
+
+}
